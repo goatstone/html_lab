28fa39ee8f8054ea79dab65bbfc9904e0d0175e9
goatstone <jose.collas@goatstone.com>
2014-12-15 13:24:34 -0800

    Add Flame object and implement, update index.hml

--- js/snap.js
+++ js/snap.js
@@ -1,8 +1,11 @@
 /* snap.js
 * */
 function Spinner() {
-    this.paper = Snap(600, 600);
+    this.paper = Snap(300, 300).attr({'id':'spinner'});
+    var backgroundColor = 'gray';
     this.path = "";
+    // this.rect = this.paper.rect(0, 0, '100%', '100%')
+    // .attr({'fill':backgroundColor})  ;
     // Element nums;
     this.nums = this.paper.text(30, 30, [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]).attr({
         fontSize: "24px",
@@ -10,7 +13,7 @@ function Spinner() {
     });
     this.animationDuration = 12000;
     this.top = 100;
-    this.left = 200;
+    this.left = 120;
     this.radius = 70
     this.spinPoint = 360;   // degree point on circle
     this.draw();
@@ -55,19 +58,84 @@ Spinner.prototype.spin = function () {
     });
 };
 
-window.onload = function () {
-    var s = new Spinner();
-    s.spin();
-//        s.roll();
+function Flame(e, opacityTo, radiusTo, x, y, speed){
+    this.element = e;
+    this.opacityTo = opacityTo? opacityTo : 0.5;
+    this.radiusTo = 5;
+    this.x = x? x:0;        
+    this.y = y? y:0;
+    this.speed = speed? speed: 2000;
+}
+Flame.prototype.engine = function (){
+    var self =  this;
+    this.element.stop().animate(
+        { transform: 's1.5t'+this.x+','+this.y+'r'+this.radiusTo, opacity:this.opacityTo},
+        this.speed,
+        function () {
+            var randNum = Math.max( 0.3, Math.random() ); 
+            // console.log('...', randNum, self);
+            self.opacityTo  = randNum; 
+            self.radiusTo = self.radiusTo > 20 ? 5 : 30 ;              
+            self.engine();    
+        }
+    );
 
-    var paper = Snap(500, 300);
+}
+
+window.onload = function () {
+    // var s = new Spinner();
+    // s.spin();
+// //        s.roll();
+
+    var paper = Snap(200, 200);
     var snapMatrix = new Snap.Matrix()
-    snapMatrix.translate(100, 100);
-    snapMatrix.rotate(20, 100, 100);
-    var text = paper.text(10, 10, "Hello World");
-    text.attr({"font-size": 30});
-    text.transform(snapMatrix);
+    snapMatrix.translate(0, 50);
+    snapMatrix.rotate(10, 100, 100);
+    // var text = paper.text(10, 10, "Hello World");
+    // text.attr({"font-size": 30});
+    // text.transform(snapMatrix);
+
+    //var flame = Snap.select('#flame path').attr('fill', 'green');
+
+    var m = new Snap.Matrix();
+    m.scale(3);
+    var flame = Snap.select('.flame path')
+    .attr('fill', 'orange')
+    .transform('s0.2t200,200');
+
+    var flame2 = flame.clone()
+    .attr('fill', 'pink')
+    .transform('t20,20');
+
+    var flame3 = flame.clone()
+    .attr('fill', 'blue')
+    .transform('t10,10');
+//    .scale(2);
+
+    var f =   new Flame(flame, 0.5, 5, 70 , 90, 1000 );
+    f.engine();
+    var f2 =   new Flame(flame2, 0.4, 5, 100, 20, 700);
+    f2.engine();
+    var f3 =   new Flame(flame3, 0.4, 5, 90, 20, 500);
+    f3.engine();
+ 
+    // var f = flame.select('#flame path');
+/*
     var moon = Snap.select('#moon').attr({fill: 'blue'});
     var circ = moon.select('circle').attr({fill: 'peru'});
     circ.transform(new Snap.Matrix().translate(20, 100));
+*/
+
 };
+
+
+                // flame.stop().animate(
+                //     {transform:'t5,5r10', opacity:'1.0'},
+                //     2000, 
+                //     function(){
+                //         // console.log('f done...');
+                //         animEngine();    
+                //     });
+                //flame.transform( 't0,0r0');
+                //flame.attr( {opacity:'1.0'});
+                //console.log(this);
